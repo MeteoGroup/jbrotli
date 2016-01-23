b333bdf18b2e7061232f2d5008990e1f3b867352
nitram509 <maki@bitkings.de>
2016-01-23 14:53:25 +0100

    refactor to use shell script for building on linux

---
+++ pipeline-tasks/maven-package-jbrotli-native-win32-x86-amd64.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PROJECT_BASE_PATH="$(dirname $0)/.."
+
+cd "$PROJECT_BASE_PATH/jbrotli-native/linux-x86-amd64" || exit $?
+mvn install || exit $?
+
+cd "$PROJECT_BASE_PATH" || exit $?
+mvn package || exit $?
