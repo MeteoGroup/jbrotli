c6ca6447e9b60e8f4bd3ad97ad8209d57ac21bb5
nitram509 <maki@bitkings.de>
2016-01-23 17:33:51 +0100

    fixed build issue in pipeline

--- pipeline-tasks/maven-package-jbrotli-native-win32-x86-amd64.sh
+++ pipeline-tasks/maven-package-jbrotli-native-win32-x86-amd64.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-PROJECT_BASE_PATH="$(dirname $0)/.."
+PROJECT_BASE_PATH=$(dirname "$0")/..
+PROJECT_BASE_PATH=$(realpath "$PROJECT_BASE_PATH")
 
 cd "$PROJECT_BASE_PATH/jbrotli-native/linux-x86-amd64" || exit $?
+chmod +x build.sh || exit $?
 mvn install || exit $?
 
 cd "$PROJECT_BASE_PATH/jbrotli" || exit $?
