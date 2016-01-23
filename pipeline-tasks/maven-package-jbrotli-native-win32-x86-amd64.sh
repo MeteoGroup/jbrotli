63fec0981e4779408d216527e6e9121c09518ad8
nitram509 <maki@bitkings.de>
2016-01-23 16:15:15 +0100

    fix path for jbrotli module

--- pipeline-tasks/maven-package-jbrotli-native-win32-x86-amd64.sh
+++ pipeline-tasks/maven-package-jbrotli-native-win32-x86-amd64.sh
@@ -5,5 +5,5 @@ PROJECT_BASE_PATH="$(dirname $0)/.."
 cd "$PROJECT_BASE_PATH/jbrotli-native/linux-x86-amd64" || exit $?
 mvn install || exit $?
 
-cd "$PROJECT_BASE_PATH" || exit $?
+cd "$PROJECT_BASE_PATH/jbrotli" || exit $?
 mvn package || exit $?
