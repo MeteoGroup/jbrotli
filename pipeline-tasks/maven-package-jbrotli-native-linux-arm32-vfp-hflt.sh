e9caf427b0d07c9bceaa960f1fe9d081f3c61042
nitram509 <maki@bitkings.de>
2016-03-05 16:25:21 +0100

    fix build script

--- pipeline-tasks/maven-package-jbrotli-native-linux-arm32-vfp-hflt.sh
+++ pipeline-tasks/maven-package-jbrotli-native-linux-arm32-vfp-hflt.sh
@@ -7,5 +7,5 @@ PROJECT_BASE_PATH=$(dirname "$0")/..
 PROJECT_BASE_PATH=$(realpath "$PROJECT_BASE_PATH")
 
 cd "$PROJECT_BASE_PATH/jbrotli-native/linux-arm32-vfp-hflt"
-chmod +x build-linux-armhf.sh
+chmod +x build*.sh
 mvn -DskipTests=true package
